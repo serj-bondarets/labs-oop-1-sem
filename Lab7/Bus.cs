8be8dc1f199f6c49ccb33dc4c166ea296fe4deed
serj-bondarets <72891902+serj-bondarets@users.noreply.github.com>
2021-02-23 00:51:33 +0300

    Add files via upload

---
+++ Lab7/Bus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public abstract class Bus
+    {
+        public abstract string Mark { get; }
+    }
+}
